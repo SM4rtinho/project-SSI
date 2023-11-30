6318681b0a8fa5a905306d2ed2756aa7673fa963
wolakw <wojtek.2017w@gmail.com>
2023-11-30 12:15:11 +0100

    push to master

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 
 public class Database {
     private static Connection connection;
-
     public static Connection getConnection() {
         if (connection == null) {
             try {
