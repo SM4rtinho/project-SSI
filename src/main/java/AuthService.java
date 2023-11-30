36aeac81843144f8269797b664c8dccf80421ab8
wolakw <wojtek.2017w@gmail.com>
2023-11-30 12:12:40 +0100

    add auth

--- src/main/java/AuthService.java
+++ src/main/java/AuthService.java
@@ -1,7 +1,9 @@
+import DTO.User;
 import org.apache.commons.codec.digest.DigestUtils;
+import DAO.UserDAO;
 
 public class AuthService {
-    public User authenticate(String email, String password, UserDao userDao) {
+    public User authenticate(String email, String password, UserDAO userDao) {
         User user = userDao.getUserByEmail(email);
 
         if (user != null && isPasswordCorrect(password, user)) {
@@ -11,7 +13,7 @@ public class AuthService {
         return null;
     }
 
-    public Boolean register(String email, String password, String name, String role, UserDao userDao) {
+    public Boolean register(String email, String password, String name, String role, UserDAO userDao) {
         if (userDao.getUserByEmail(email) != null) {
             // User already exists
             return false;
